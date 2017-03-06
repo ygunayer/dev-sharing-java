36dd6f67a87e7ba733d820b7f5103d41ce554281
Yalin Gunayer <yalin.gunayer@gmail.com>
2017-03-06 22:48:35 +0300

    Demonstrate polymorphism

--- src/main/java/com/emlakjet/devsharing/Main.java
+++ src/main/java/com/emlakjet/devsharing/Main.java
@@ -1,14 +1,20 @@
 package com.emlakjet.devsharing;
 
 import com.emlakjet.devsharing.marketplace.Emlakjet;
+import com.emlakjet.devsharing.marketplace.RealtyMarketplace;
 import com.emlakjet.devsharing.marketplace.Sahibinden;
 import com.emlakjet.devsharing.marketplace.Zingat;
 
 public class Main {
     public static void main(String[] args) {
-        Emlakjet emlakjet = new Emlakjet();
-        Zingat zingat = new Zingat();
-        Sahibinden sahibinden = new Sahibinden();
-        System.out.println(emlakjet.getSlogan() + " <-> " + zingat.getSlogan() + " <-> " + sahibinden.getSlogan());
+        RealtyMarketplace[] marketplaces = new RealtyMarketplace[] { new Emlakjet(), new Sahibinden(), new Zingat() };
+
+        for (RealtyMarketplace market : marketplaces) {
+            printMarketplace(market);
+        }
+    }
+
+    public static void printMarketplace(RealtyMarketplace market) {
+        System.out.println(market.getSlogan() + " - " + market.getName() + " (est. " + market.getEstablished() + ")");
     }
 }
